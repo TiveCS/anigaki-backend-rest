4475706217c1bbea1d6017cd7f6b12ecffcb2ce6
TiveCS <ahmadfathanhanif@gmail.com>
2022-08-07 17:18:32 +0700

    preparation

---
+++ prisma/migrations/20220807101608_init/migration.sql
@@ -0,0 +1,76 @@
+-- CreateTable
+CREATE TABLE "admins" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "animes" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "releaseDate" TIMESTAMP(3) NOT NULL,
+    "postedBy" INTEGER NOT NULL,
+
+    CONSTRAINT "animes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "anime_posters" (
+    "id" SERIAL NOT NULL,
+    "animeId" INTEGER NOT NULL,
+    "posterUrl" TEXT NOT NULL,
+
+    CONSTRAINT "anime_posters_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "anime_profiles" (
+    "id" SERIAL NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "animeId" INTEGER NOT NULL,
+    "altTitle" TEXT,
+    "genres" TEXT[],
+
+    CONSTRAINT "anime_profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "episodes" (
+    "id" SERIAL NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "animeId" INTEGER NOT NULL,
+    "episodeNumber" INTEGER NOT NULL,
+    "download1080p" TEXT,
+    "download720p" TEXT,
+    "download480p" TEXT,
+    "download360p" TEXT,
+
+    CONSTRAINT "episodes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_username_key" ON "admins"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "anime_posters_animeId_key" ON "anime_posters"("animeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "anime_profiles_animeId_key" ON "anime_profiles"("animeId");
+
+-- AddForeignKey
+ALTER TABLE "animes" ADD CONSTRAINT "animes_postedBy_fkey" FOREIGN KEY ("postedBy") REFERENCES "admins"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "anime_posters" ADD CONSTRAINT "anime_posters_animeId_fkey" FOREIGN KEY ("animeId") REFERENCES "animes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "anime_profiles" ADD CONSTRAINT "anime_profiles_animeId_fkey" FOREIGN KEY ("animeId") REFERENCES "animes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "episodes" ADD CONSTRAINT "episodes_animeId_fkey" FOREIGN KEY ("animeId") REFERENCES "animes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
