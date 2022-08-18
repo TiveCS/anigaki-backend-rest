61abc21a6352fbef0e379fa31529a91bf200b489
TiveCS <ahmadfathanhanif@gmail.com>
2022-08-18 09:53:58 +0700

    set httpsOption

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import fs from 'fs';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('../secret/key.pem'),
+      cert: fs.readFileSync('../secret/cert.pem'),
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
-    })
+    }),
   );
 
   const origin: string | RegExp = process.env.FRONTEND_URL || '*';
